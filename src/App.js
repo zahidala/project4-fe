1b89c58818789e551c16c6451d892c12f96e49d0
Nadia Husain <nadia.neerooz@gmail.com>
2023-04-09 10:10:20 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
 import React from 'react'
 import {CssBaseline, Grid} from '@material-ui/core'
-
-import Header from './Components/Header/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+// import Header from './Components/Header/Header'
 import List from './Components/List/List'
 import Map from './Components/Map/Map'
 
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+
 export default function App() {
   return (
+    // <>
+    //   <CssBaseline />
+    //   <Header />
+    //   <Grid container spacing={3} style={{ width: '100%' }}>
+    //     <Grid item xs={12} md={4}>
+    //       <List />
+    //     </Grid>
+    //     <Grid item xs={12} md={8}>
+    //       <Map />
+    //     </Grid>
+    //   </Grid>
+    // </>
     <>
-      <CssBaseline />
-      <Header />
-      <Grid container spacing={3} style={{ width: '100%' }}>
-        <Grid item xs={12} md={4}>
-          <List />
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Map />
-        </Grid>
-      </Grid>
-    </>
+    <Navbar  expand="lg" className='color-nav'>
+      <Container fluid>
+      <Navbar.Brand href="#">Trip Planner</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+      
+        <Router>
+          
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            
+              <Nav.Link to='/'>Home</Nav.Link>
+              <Nav.Link to="/signup">Sign Up </Nav.Link>
+              <Nav.Link to="/signin">Sign In</Nav.Link>
+              <Nav.Link to="/logout" >Sign Out</Nav.Link>
+            
+            </Nav>
+          <div>
+            <Routes>
+              <Route path="/" ></Route>
+              <Route path="/signup" ></Route>
+              <Route path="/signin" ></Route>
+            </Routes>
+          </div>
+
+        </Router>
+        </Navbar.Collapse>
+        </Container>
+        </Navbar>
+
+        
+  <div
+       id="intro"
+       class="bg-image"
+       >
+    <div class="mask text-white">
+      <div class="container d-flex align-items-center text-center h-100">
+        <div>
+          <p>Plan you next trip with us!</p>
+        </div>
+        <div>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  </div>
+  </>
   )
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,40 @@
-import React from 'react'
-import { Autocomplete } from '@react-google-maps/api'
-import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
-import SearchIcon from '@material-ui/icons/Search'
+// import React from 'react'
+// import { Autocomplete } from '@react-google-maps/api'
+// import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
+// import SearchIcon from '@material-ui/icons/Search'
+// import { Button, Container, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
+// import useStyles from './styles'
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import useStyles from './styles'
-
-export default function Header() {
-  const classes = useStyles()
+// export default function Header() {
+//   const classes = useStyles()
   
-  return (
-    <AppBar position="static">
-    <Toolbar className={classes.toolbar}>
-      <Typography variant="h5" className={classes.title}>
-        Travel Advisor
-      </Typography>
-      <Box display="flex">
-        <Typography variant="h6" className={classes.title}>
-          Explore new places
-        </Typography>
-        {/* <Autocomplete> */}
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-          </div>
-        {/* </Autocomplete> */}
-      </Box>
-    </Toolbar>
-  </AppBar>
-  )
-}
+//   return (
+//     <AppBar position="static">
+//     <Toolbar className={classes.toolbar}>
+//       <Typography variant="h5" className={classes.title}>
+//         Travel Advisor
+//       </Typography>
+//       <Box display="flex">
+//         <Typography variant="h6" className={classes.title}>
+//           Explore new places
+//         </Typography>
+//         {/* <Autocomplete> */}
+//           <div className={classes.search}>
+//             <div className={classes.searchIcon}>
+//               <SearchIcon />
+//             </div>
+//             <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+//           </div>
+//         {/* </Autocomplete> */}
+//       </Box>
+//     </Toolbar>
+//   </AppBar>
+//   )
+  
+// }
