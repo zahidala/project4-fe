07bb8fdf0a1d3cf50e8c1857b2b62dcd40a1e7c0
Nadia Husain <nadia.neerooz@gmail.com>
2023-04-09 15:54:02 +0300

    added list and fixed map a bit with @jehanalik @zahidala @nooralhajri @hajrm

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -8,47 +8,51 @@ import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 
 import useStyles from './styles'
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
+// const containerStyle = {
+//   width: '400px',
+//   height: '400px'
+// };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+// const center = {
+//   lat: -3.745,
+//   lng: -38.523
+// };
 
-export default function Map() {
+export default function Map({ setCoordinates, setBounds, coordinates }) {
   const classes = useStyles()
   const isMobile = useMediaQuery('(min-width:600px)')
-  const coordinates = { lan: 0, lng: 0}
   return (
-    // <div className={classes.mapContainer}>
-    //   <GoogleMapReact 
-    //   bootstrapURLKeys={{ key : process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
-    //   defaultCenter={coordinates}
-    //   center={coordinates}
-    //   defaultZoom={14}
-    //   margin={[50, 50, 50, 50]}
-    //   options={''}
-    //   onChange={''}
-    //   onChildClick={''}
-    //   >
-    //   </GoogleMapReact>
-    // </div>
-
-    <LoadScript
-      googleMapsApiKey= {process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
+    <div className={classes.mapContainer}>
+      <GoogleMapReact 
+      bootstrapURLKeys={{ key : process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
+      defaultCenter={coordinates}
+      center={coordinates}
+      defaultZoom={14}
+      margin={[50, 50, 50, 50]}
+      options={''}
+      onChange={(e) => {
+        console.log(e)
+        setCoordinates({ lat: e.center.lat, lng: e.center.lng})
+        setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+      }}
+      onChildClick={''}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
+      </GoogleMapReact>
+    </div>
+
+    // <LoadScript
+    //   googleMapsApiKey= {process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+    // >
+    //   <GoogleMap
+    //     mapContainerStyle={containerStyle}
+    //     center={center}
+    //     zoom={10}
+    //     onChange
+    //   >
+    //     { /* Child components, such as markers, info windows, etc. */ }
+    //     <></>
+    //   </GoogleMap>
+    // </LoadScript>
 
   )
 }
--- src/Components/PlaceDetails/PlaceDetails.js
+++ src/Components/PlaceDetails/PlaceDetails.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function PlaceDetails({ place }) {
   return (
-    <h1>PlaceDetails</h1>
+    <h1>{place.name}</h1>
   )
 }
