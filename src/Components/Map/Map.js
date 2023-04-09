96b7d7376cc8334a7ef166d479d57abce9389a1e
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-04-09 12:06:22 +0300

    figuring out map render... @nadia-husain

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
+
+import useStyles from './styles'
 
 export default function Map() {
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width:600px)')
   const coordinates = { lan: 0, lng: 0}
   return (
-    <div>
+    <div className={classes.mapContainer}>
       <GoogleMapReact 
-      bootstrapURLKeys={{ key : ''}}
+      bootstrapURLKeys={{ key : process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
       defaultCenter={coordinates}
       center={coordinates}
       defaultZoom={14}
