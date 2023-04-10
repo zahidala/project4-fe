05cc112a586c1977d3dff8a5bc3cb9d741fe2021
jehan ali <jeehankhosro@gmail.com>
2023-04-10 12:39:56 +0300

    conflict

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -4,11 +4,8 @@ import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, 
 import PlaceDetails from '../PlaceDetails/PlaceDetails'
 
 import useStyles from './styles'
-<<<<<<< HEAD
-=======
 
 import {Form, Container} from 'react-bootstrap'
->>>>>>> 0c7fc2216f2a77019a46c40f6be654e8d40f3750
 
 export default function List() {
   const classes = useStyles()
@@ -21,35 +18,6 @@ export default function List() {
   ]
 
   return (
-<<<<<<< HEAD
-    <div className={classes.container}>
-      <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
-      <FormControl className={classes.formControl}>
-        <InputLabel>Type</InputLabel>
-        <Select value={type} onchange={(e) => setType(e.target.value)}>
-          <MenuItem value="restaurants">Restaurants</MenuItem>
-          <MenuItem value="hotels">Hotels</MenuItem>
-          <MenuItem value="attractions">Attractions</MenuItem>
-        </Select>
-      </FormControl>
-
-      <FormControl className={classes.formControl}>
-        <InputLabel>Type</InputLabel>
-        <Select value={rating} onchange={(e) => setRating(e.target.value)}>
-          <MenuItem value={0}>All</MenuItem>
-          <MenuItem value={3}>Above 3.0</MenuItem>
-          <MenuItem value={4}>Above 4.0</MenuItem>
-          <MenuItem value={4.5}>Above 4.5</MenuItem>
-        </Select>
-      </FormControl>
-      <Grid container spacing={3} className={classes.list}>
-        {places?.map((place, i) => (
-          <Grid item key={i} xs={12}>
-            <PlaceDetails place={place} />
-          </Grid> 
-        ))}
-      </Grid>
-=======
     // <div className={classes.container}>
     //   <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
     //   <FormControl className={classes.formControl}>
@@ -103,7 +71,6 @@ export default function List() {
     </Container>
 
 
->>>>>>> 0c7fc2216f2a77019a46c40f6be654e8d40f3750
     </div>
   )
 }
