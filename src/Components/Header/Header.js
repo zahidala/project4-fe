ae1525071605cb02ac776ffaa58dccdff9d4a7ec
Nadia Husain <nadia.neerooz@gmail.com>
2023-04-11 01:08:33 +0300

    attempt to add autocomplete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,40 +1,68 @@
-// import React from 'react'
-// import { Autocomplete } from '@react-google-maps/api'
-// import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
-// import SearchIcon from '@material-ui/icons/Search'
-// import { Button, Container, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
-// import useStyles from './styles'
-// import Button from 'react-bootstrap/Button';
-// import Container from 'react-bootstrap/Container';
-// import Form from 'react-bootstrap/Form';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import useStyles from './styles'
+import Map from '../Map/Map'
+import { useState, useEffect } from 'react'
+import List from '../List/List'
+import { getPlacesData } from '../../api';
 
-// export default function Header() {
-//   const classes = useStyles()
+export default function Header() {
+    const [places, setPlaces] = useState([])
+  const [coordinates, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState({})
   
-//   return (
-//     <AppBar position="static">
-//     <Toolbar className={classes.toolbar}>
-//       <Typography variant="h5" className={classes.title}>
-//         Travel Advisor
-//       </Typography>
-//       <Box display="flex">
-//         <Typography variant="h6" className={classes.title}>
-//           Explore new places
-//         </Typography>
-//         {/* <Autocomplete> */}
-//           <div className={classes.search}>
-//             <div className={classes.searchIcon}>
-//               <SearchIcon />
-//             </div>
-//             <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-//           </div>
-//         {/* </Autocomplete> */}
-//       </Box>
-//     </Toolbar>
-//   </AppBar>
-//   )
-  
-// }
+  // const [autocomplete, setAutocomplete] = useState(null)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+      setCoordinates({ lat: latitude, lng: longitude })
+
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log(coordinates, bounds)
+    getPlacesData(bounds.sw, bounds.ne)
+    .then((data) => {
+      console.log(data)
+      setPlaces(data)
+    })
+  }, [coordinates, bounds])
+
+  return (
+    <>
+    <div id="intro" class="bg-image">
+    <div class="mask text-white">
+      <div class="container d-flex align-items-center text-center h-100">
+        <div>
+          <p>Plan you next trip with us!</p>
+        </div>
+        <div>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            {/* <Autocomplete> */}
+            <Button variant="outline-success">Search</Button>
+            {/* </Autocomplete> */}
+          </Form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places={places}/>
+    <Container>
+      <List places={places} />
+    </Container>
+  </div>
+  </>
+)
+}
